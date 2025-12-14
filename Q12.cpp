6df12f6dbac86d4c05ba72716d60465f0cf2f2da
krish-ezhu <krishezhu2005@gmail.com>
2025-12-14 14:52:52 +0530

    Add files via upload

---
+++ Q12.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    int num1,num2,sum;
+    cout<<"Enter a number 1 : ";
+    cin>>num1;
+    cout<<"Enter a number 2 : ";
+    cin>>num2;
+    sum=num1*num2;
+    cout<<sum;
+    return 0;
+}
