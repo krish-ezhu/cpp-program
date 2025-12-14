11c441e5dd518b51a0ee158fbf212f19bb8e7956
krish-ezhu <krishezhu2005@gmail.com>
2025-12-14 15:02:34 +0530

    Add files via upload

---
+++ Q14.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    int num1,num2,quatient;
+    cout<<"Enter a number 1 : ";
+    cin>>num1;
+    cout<<"Enter a number 2 : ";
+    cin>>num2;
+    quatient=num1/num2;
+    cout<<quatient;
+    return 0;
+}
