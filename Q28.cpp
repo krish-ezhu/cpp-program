3542942816ba0adb530bb8817d1ac379d9f73c8a
Krishnakumar Elumalai <krishezhu2005@gmail.com>
2025-12-15 20:46:20 +0530

    Add triangle type checker in Q28.cpp
    This program checks the type of triangle based on the lengths of its sides.

---
+++ Q28.cpp
@@ -0,0 +1,27 @@
+//Write a C++ Program to check whether a triangle is equilateral, isosceles, or scalene.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+
+    cout << "Enter three sides of the triangle: ";
+    cin >> a >> b >> c;
+
+    
+    if (a + b > c && a + c > b && b + c > a) {
+
+        if (a == b && b == c)
+            cout << "The triangle is Equilateral";
+        else if (a == b || b == c || a == c)
+            cout << "The triangle is Isosceles";
+        else
+            cout << "The triangle is Scalene";
+
+    } else {
+        cout << "Not a valid triangle";
+    }
+
+    return 0;
+}
